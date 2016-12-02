80d2f15cffc5bc772d33a0d7b316975a9d13edfc
Daniel Loth <lothyis@gmail.com>
2016-12-03 10:02:11 +1100

    Fixes #2.
    Added unit testing around GamePooledByteBufferAllocator.

--- src/OpenClassic.Server/Networking/GamePooledByteBufferAllocator.cs
+++ src/OpenClassic.Server/Networking/GamePooledByteBufferAllocator.cs
@@ -7,9 +7,9 @@ namespace OpenClassic.Server.Networking
     {
         readonly IByteBufferAllocator DelegateAllocator = PooledByteBufferAllocator.Default;
 
-        const int BufferFrontPadding = 4;
+        public const int BufferFrontPadding = 4;
         const int DefaultInitialCapacity = 256; // This is the same as DotNetty's (at least at 3 Dec 2016).
-        const int InitialCapacityPlusPadding = BufferFrontPadding + DefaultInitialCapacity;
+        const int PaddingMask = unchecked((int)0xAAAAAAAA);
 
         public static readonly IByteBufferAllocator Default = new GamePooledByteBufferAllocator();
 
@@ -19,6 +19,10 @@ namespace OpenClassic.Server.Networking
 
         public IByteBuffer Buffer(int initialCapacity, int maxCapacity)
         {
+            // If the capacity values are negative then clamp them at zero.
+            initialCapacity = initialCapacity >= 0 ? initialCapacity : 0;
+            maxCapacity = maxCapacity >= 0 ? maxCapacity : 0;
+
             Debug.Assert(initialCapacity >= 0);
             Debug.Assert(maxCapacity >= 0);
 
@@ -27,14 +31,26 @@ namespace OpenClassic.Server.Networking
                 return DelegateAllocator.Buffer(0, 0); // Will returned a reusable EmptyBuffer.
             }
 
-            var initialCapacityPlusPadding = initialCapacity + BufferFrontPadding;
-            var maxCapacityPlusPadding = maxCapacity + BufferFrontPadding;
+            long maxCapLong = (long) maxCapacity + BufferFrontPadding;
+            long initCapLong = (long)initialCapacity + BufferFrontPadding;
 
-            var buffer = DelegateAllocator.Buffer(initialCapacityPlusPadding, maxCapacityPlusPadding);
+            var clampedMaxCapacity = maxCapLong > int.MaxValue ? int.MaxValue : (int) maxCapLong;
+            var clampedInitialCapacity = initCapLong > int.MaxValue ? int.MaxValue : (int)initCapLong;
+
+            Debug.Assert(clampedMaxCapacity >= 0 && clampedMaxCapacity <= int.MaxValue);
+            Debug.Assert(clampedInitialCapacity >= 0 && clampedInitialCapacity <= int.MaxValue);
+
+            var buffer = DelegateAllocator.Buffer(clampedInitialCapacity, clampedMaxCapacity);
             var writerIndex = buffer.WriterIndex;
             var writerIndexAfterPadding = writerIndex + BufferFrontPadding;
 
+            buffer.SetInt(writerIndex, PaddingMask);
+
             buffer.SetWriterIndex(writerIndexAfterPadding);
+            buffer.MarkWriterIndex();
+
+            buffer.SetReaderIndex(writerIndexAfterPadding);
+            buffer.MarkReaderIndex();
 
             return buffer;
         }
---
+++ test/OpenClassic.Server.Tests/Networking/GamePooledByteBufferAllocatorTests.cs
@@ -0,0 +1,119 @@
+﻿using DotNetty.Buffers;
+using OpenClassic.Server.Networking;
+using Xunit;
+
+namespace OpenClassic.Server.Tests.Networking
+{
+    public class GamePooledByteBufferAllocatorTests
+    {
+        [Fact]
+        public void AllocatesBuffer()
+        {
+            var buffer = GamePooledByteBufferAllocator.Default.Buffer();
+
+            Assert.NotNull(buffer);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(256)]
+        [InlineData(4096)]
+        public void AllocatesBufferWithCorrectInitialCapacity(int initialCapacity)
+        {
+            var buffer = GamePooledByteBufferAllocator.Default.Buffer(initialCapacity);
+
+            var expectedInitialCapacity = initialCapacity + GamePooledByteBufferAllocator.BufferFrontPadding;
+            var actualInitialCapacity = buffer.Capacity;
+
+            Assert.NotNull(buffer);
+            Assert.Equal(expectedInitialCapacity, actualInitialCapacity);
+        }
+
+        [Theory]
+        [InlineData(0, 32)]
+        [InlineData(0, 256)]
+        [InlineData(1, 1)]
+        [InlineData(4096, 8192)]
+        public void AllocatesBufferWithCorrectMaxCapacity(int initialCapacity, int maxCapacity)
+        {
+            var buffer = GamePooledByteBufferAllocator.Default.Buffer(initialCapacity, maxCapacity);
+
+            var paddedInitialCapacity = initialCapacity + GamePooledByteBufferAllocator.BufferFrontPadding;
+            var paddedMaxCapacity = maxCapacity + GamePooledByteBufferAllocator.BufferFrontPadding;
+
+            Assert.NotNull(buffer);
+            Assert.Equal(paddedInitialCapacity, buffer.Capacity);
+            Assert.Equal(paddedMaxCapacity, buffer.MaxCapacity);
+        }
+
+        [Theory]
+        [InlineData(int.MinValue, int.MinValue)]
+        [InlineData(-1, -1)]
+        public void AllocatesEmptyBufferOnNegativeCapacity(int initialCapacity, int maxCapacity)
+        {
+            var buffer = GamePooledByteBufferAllocator.Default.Buffer(initialCapacity, maxCapacity);
+
+            Assert.NotNull(buffer);
+            Assert.Equal(0, buffer.Capacity);
+            Assert.Equal(0, buffer.MaxCapacity);
+            Assert.IsType<EmptyByteBuffer>(buffer);
+        }
+
+        [Fact]
+        public void AllocatesEmptyBufferOnZeroCapacity()
+        {
+            var buffer = GamePooledByteBufferAllocator.Default.Buffer(0, 0);
+
+            Assert.NotNull(buffer);
+            Assert.Equal(0, buffer.Capacity);
+            Assert.Equal(0, buffer.MaxCapacity);
+            Assert.IsType<EmptyByteBuffer>(buffer);
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(32, 32)]
+        [InlineData(256, 4096)]
+        public void WritesBitMaskInPaddingArea(int initialCapacity, int maxCapacity)
+        {
+            var buffer = GamePooledByteBufferAllocator.Default.Buffer(initialCapacity, maxCapacity);
+
+            var expectedMask = unchecked((int)0xAAAAAAAA);
+            var actualMask = buffer.GetInt(0);
+
+            Assert.NotNull(buffer);
+            Assert.Equal(expectedMask, actualMask);
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(32, 32)]
+        [InlineData(256, 4096)]
+        public void SetsReaderIndexToFirstByteAfterPaddingArea(int initialCapacity, int maxCapacity)
+        {
+            var buffer = GamePooledByteBufferAllocator.Default.Buffer(initialCapacity, maxCapacity);
+
+            var expectedReaderIndex = GamePooledByteBufferAllocator.BufferFrontPadding;
+            var actualReaderIndex = buffer.ReaderIndex;
+
+            Assert.NotNull(buffer);
+            Assert.Equal(expectedReaderIndex, actualReaderIndex);
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(32, 32)]
+        [InlineData(256, 4096)]
+        public void SetsWriterIndexToFirstByteAfterPaddingArea(int initialCapacity, int maxCapacity)
+        {
+            var buffer = GamePooledByteBufferAllocator.Default.Buffer(initialCapacity, maxCapacity);
+
+            var expectedWriterIndex = GamePooledByteBufferAllocator.BufferFrontPadding;
+            var actualWriterIndex = buffer.WriterIndex;
+
+            Assert.NotNull(buffer);
+            Assert.Equal(expectedWriterIndex, actualWriterIndex);
+        }
+    }
+}
