9d8f4faea6cd598bfd4e41e87fde45388072f9f6
Daniel Loth <lothyis@gmail.com>
2016-12-02 23:35:33 +1100

    Added DryIOC for dependency injection.

---
+++ src/OpenClassic.Server/Configuration/DependencyResolver.cs
@@ -0,0 +1,25 @@
+﻿using DryIoc;
+using System;
+
+namespace OpenClassic.Server.Configuration
+{
+    public static class DependencyResolver
+    {
+        public static IContainer Current { get; }
+
+        static DependencyResolver()
+        {
+            var container = new Container();
+
+            RegisterDependencies(container);
+
+            Current = container;
+        }
+
+        static void RegisterDependencies(IContainer container)
+        {
+            if (container == null)
+                throw new ArgumentNullException(nameof(container));
+        }
+    }
+}
