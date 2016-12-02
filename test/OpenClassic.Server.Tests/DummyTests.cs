e389b97adef338ca98a0421234dc13bbfb324704
Daniel Loth <lothyis@gmail.com>
2016-12-02 21:21:54 +1100

    -ADD: Test project.

---
+++ test/OpenClassic.Server.Tests/DummyTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace OpenClassic.Server.Tests
+{
+    public class DummyTests
+    {
+        [Fact]
+        public void Blah()
+        {
+            Assert.Null(null);
+        }
+
+        [Theory]
+        [InlineData(unchecked ((int)-2147483649))]
+        [InlineData(int.MaxValue)]
+        [InlineData(2)]
+        public void Blah2(int a)
+        {
+            Assert.Null(null);
+        }
+    }
+}
