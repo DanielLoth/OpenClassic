1223578cfa2e6e496029b763731379b5c9537fc7
Daniel Loth <lothyis@gmail.com>
2016-12-04 20:40:11 +1100

    GameMessageDecoder improvements. Decoding a packet with 1-byte length field seems to work.
    Added unit tests class for GameMessageDecoder.

--- src/OpenClassic.Server/Networking/GameMessageDecoder.cs
+++ src/OpenClassic.Server/Networking/GameMessageDecoder.cs
@@ -11,74 +11,77 @@ namespace OpenClassic.Server.Networking
     {
         public override bool IsSharable => false;
 
-        public GameMessageDecoder(IChannel channel)
-        {
-            Debug.Assert(channel != null);
-        }
-
         protected override void Decode(IChannelHandlerContext context, IByteBuffer input, List<object> output)
         {
             Debug.Assert(context != null);
             Debug.Assert(input != null);
             Debug.Assert(output != null);
 
-            input.MarkReaderIndex();
-
-            if (input.ReadableBytes < 2)
+            while (true)
             {
-                return;
-            }
+                input.MarkReaderIndex();
+                var firstByteInPacketIndex = input.ReaderIndex;
 
-            var payloadLength = (int)input.ReadByte();
-            var isTwoByteLen = false;
-            if (payloadLength >= 160)
-            {
-                payloadLength = (payloadLength - 160) * 256 + input.ReadByte();
-                isTwoByteLen = true;
-            }
+                if (input.ReadableBytes < 2)
+                {
+                    break;
+                }
 
-            var inboundDataReadyForProcessing = input.ReadableBytes >= payloadLength;
-            if (!inboundDataReadyForProcessing)
-            {
-                input.ResetReaderIndex();
-                return;
-            }
+                var payloadLengthIncludingOpcode = (int)input.ReadByte();
+                var isTwoByteLen = false;
+                if (payloadLengthIncludingOpcode >= 160)
+                {
+                    payloadLengthIncludingOpcode = (payloadLengthIncludingOpcode - 160) * 256 + input.ReadByte();
+                    isTwoByteLen = true;
+                }
 
-            var packetLenExcludingOpcode = payloadLength - 1;
-            if (packetLenExcludingOpcode == 0)
-            {
-                var opcode = input.ReadByte();
-                var emptyBuffer = context.Allocator.Buffer(0, 0);
-                var packet = new Packet(opcode, emptyBuffer);
+                var inboundDataReadyForProcessing = input.ReadableBytes >= payloadLengthIncludingOpcode;
+                if (!inboundDataReadyForProcessing)
+                {
+                    input.ResetReaderIndex();
+                    break;
+                }
 
-                output.Add(packet);
-            }
-            else if (!isTwoByteLen)
-            {
-                var lastPayloadByte = input.ReadByte();
-                var opcode = input.ReadByte();
-                var newBuffer = context.Allocator.Buffer(payloadLength);
-                var dataLen = packetLenExcludingOpcode - 1;
+                var payloadLengthExcludingOpcode = payloadLengthIncludingOpcode - 1;
+                if (payloadLengthExcludingOpcode == 0)
+                {
+                    var opcode = input.ReadByte();
 
-                newBuffer.SetByte(0, opcode);
-                newBuffer.SetShort(1, dataLen);
+                    //input.SetOpcode(opcode);
+                    //input.SetPayloadLength(0);
 
-                input.ReadBytes(newBuffer, dataLen);
-                newBuffer.WriteByte(lastPayloadByte);
+                    output.Add(input.Retain());
+                }
+                else if (!isTwoByteLen)
+                {
+                    var lastPayloadByte = input.ReadByte();
+                    var firstByteToMoveLeftIndex = input.ReaderIndex;
+                    var opcode = input.ReadByte();
 
-                var hex = ByteBufferUtil.PrettyHexDump(newBuffer);
-                var hex2 = ByteBufferUtil.HexDump(newBuffer);
+                    var bytesToMoveLeft = payloadLengthExcludingOpcode;
 
-                output.Add(newBuffer);
-            }
-            else
-            {
-                var opcode = input.ReadByte();
-                var newBuffer = context.Allocator.Buffer(payloadLength);
-                input.ReadBytes(newBuffer, packetLenExcludingOpcode);
+                    var writeIndex = firstByteInPacketIndex;
+                    for (int i = 0, readIndex = firstByteToMoveLeftIndex; i < bytesToMoveLeft; i++, readIndex++, writeIndex++)
+                    {
+                        input.SetByte(writeIndex, input.GetByte(readIndex));
+                    }
+                    input.SetByte(writeIndex, lastPayloadByte);
 
-                var packet = new Packet(opcode, newBuffer);
-                output.Add(packet);
+                    var packet = input.Slice(firstByteInPacketIndex, payloadLengthIncludingOpcode);
+                    packet.SetReaderIndex(1);
+                    packet.MarkReaderIndex();
+
+                    output.Add(packet.Retain());
+                }
+                else
+                {
+                    var opcode = input.ReadByte();
+                    var newBuffer = context.Allocator.Buffer(payloadLengthIncludingOpcode);
+                    input.ReadBytes(newBuffer, payloadLengthExcludingOpcode);
+
+                    var packet = new Packet(opcode, newBuffer);
+                    output.Add(input.Retain());
+                }
             }
         }
     }
---
+++ test/OpenClassic.Server.Tests/Networking/GameMessageDecoderTests.cs
@@ -0,0 +1,69 @@
+﻿using DotNetty.Buffers;
+using DotNetty.Transport.Channels.Embedded;
+using OpenClassic.Server.Networking;
+using System;
+using Xunit;
+
+namespace OpenClassic.Server.Tests.Networking
+{
+    public class GameMessageDecoderTests
+    {
+        private byte[] RequestSession = { 2, 55, 32 };
+        private byte[] SendPrivacySetings = { 5, 4, 64, 1, 2, 3 };
+
+        [Fact]
+        public void OnePacketSingleByteLen()
+        {
+            var channel = new EmbeddedChannel(new GameMessageDecoder());
+            var buffer = Unpooled.WrappedBuffer(RequestSession);
+
+            channel.WriteInbound(buffer);
+
+            var result = channel.ReadInbound<IByteBuffer>();
+            var result2 = channel.ReadInbound<object>(); // Returns null, as there's no second object returned in the List<object>.
+        }
+
+        [Fact]
+        public void DecodesMultiplePacketsWith1ByteLenInOneInvocation()
+        {
+            var channel = new EmbeddedChannel(new GameMessageDecoder());
+            var buffer = Unpooled.Buffer();
+            const int packetCount = 10;
+
+            for (var i = 0; i < packetCount; i++)
+            {
+                buffer.WriteBytes(RequestSession);
+            }
+
+            channel.WriteInbound(buffer);
+
+            for (var i = 0; i < packetCount; i++)
+            {
+                var packet = channel.ReadInbound<IByteBuffer>();
+
+                Assert.NotNull(packet);
+                Assert.Equal(1, packet.ReaderIndex);
+                Assert.Equal(1, packet.ReadableBytes);
+                Assert.Equal(1, packet.GetPayloadLength());
+                Assert.Equal(32, packet.GetOpcode());
+                Assert.Equal(55, packet.ReadByte());
+                Assert.True(packet.ReferenceCount > 1);
+            }
+
+            // Verify that there are no more results (there shouldn't be at this point).
+            Assert.Null(channel.ReadInbound<IByteBuffer>());
+
+            Assert.Equal(packetCount, buffer.ReferenceCount);
+        }
+
+        private static IByteBuffer GetPaddedByteBuffer(byte[] contents)
+        {
+            if (contents == null) throw new ArgumentNullException();
+
+            var buffer = GamePooledByteBufferAllocator.Default.Buffer(contents.Length);
+            buffer.WriteBytes(contents);
+
+            return buffer;
+        }
+    }
+}
