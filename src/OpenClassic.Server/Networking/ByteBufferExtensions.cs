aa53a8136177c0e10389b045f9047f26e9626327
Daniel Loth <lothyis@gmail.com>
2016-12-04 20:39:27 +1100

    Added IByteBuffer extension methods to retrieve the packet OpCode and payload length.

---
+++ src/OpenClassic.Server/Networking/ByteBufferExtensions.cs
@@ -0,0 +1,22 @@
+﻿using DotNetty.Buffers;
+using System.Diagnostics;
+
+namespace OpenClassic.Server.Networking
+{
+    public static class ByteBufferExtensions
+    {
+        public static int GetOpcode(this IByteBuffer buffer)
+        {
+            Debug.Assert(buffer != null);
+
+            return buffer.GetByte(0);
+        }
+
+        public static int GetPayloadLength(this IByteBuffer buffer)
+        {
+            Debug.Assert(buffer != null);
+
+            return buffer.Capacity - 1;
+        }
+    }
+}
