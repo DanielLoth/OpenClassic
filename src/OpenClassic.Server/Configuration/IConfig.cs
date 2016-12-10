0fcc1807611d525d6babca04b1fe3d6a85b2406a
Daniel Loth <lothyis@gmail.com>
2016-12-11 09:13:28 +1100

    Added server protocol and RSA keypair settings to IConfig.

--- src/OpenClassic.Server/Configuration/IConfig.cs
+++ src/OpenClassic.Server/Configuration/IConfig.cs
@@ -5,5 +5,11 @@
         bool BoolTest { get; set; }
         int IntTest { get; set; }
         string StringTest { get; set; }
+
+        string ServerProtocol { get; set; }
+
+        string RsaEncryptionKey { get; set; }
+        string RsaDecryptionKey { get; set; }
+        string RsaModulus { get; set; }
     }
 }
