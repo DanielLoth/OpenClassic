5fd40bdcec978eba7496e36d34802a9bcaddebbc
Daniel Loth <lothyis@gmail.com>
2016-12-16 22:37:25 +1100

    Added SingleObjectOrArrayJsonConverter class. This is used with the Json.NET library.
    It's useful because the JSON game data isn't always consistent. Sometimes there's a single object in the data, other times an array.
    This class handles both by returning a List<T> or zero-to-many objects.

---
+++ src/OpenClassic.Server/Util/SingleObjectOrArrayJsonConverter.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace OpenClassic.Server.Util
+{
+    public class SingleObjectOrArrayJsonConverter<T> : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return (objectType == typeof(List<T>));
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var results = new List<T>();
+
+            var token = JToken.Load(reader);
+            if (token.Type == JTokenType.Array)
+            {
+                var multipleObjects = token.ToObject<List<T>>();
+
+                results.AddRange(multipleObjects);
+            }
+            else if (token.Type == JTokenType.Object)
+            {
+                var singleObject = token.ToObject<T>();
+
+                results.Add(singleObject);
+            }
+
+            return results;
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+
+        }
+    }
+}
