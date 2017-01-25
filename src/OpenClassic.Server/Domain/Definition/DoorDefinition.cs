3cf57692924d003081f638ea89ad0e093d5d33fc
Daniel Loth <lothyis@gmail.com>
2017-01-25 22:21:52 +1100

    Added door and tile definition classes.

---
+++ src/OpenClassic.Server/Domain/Definition/DoorDefinition.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenClassic.Server.Domain.Definition
+{
+    public class DoorDefinition
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Command1 { get; set; }
+
+        public string Command2 { get; set; }
+
+        public string ModelVar1 { get; set; }
+
+        public string ModelVar2 { get; set; }
+
+        public string ModelVar3 { get; set; }
+
+        public int DoorType { get; set; }
+
+        public int Unknown { get; set; }
+    }
+}
---
+++ src/OpenClassic.Server/Domain/Definition/TileDefinition.cs
@@ -0,0 +1,11 @@
+﻿namespace OpenClassic.Server.Domain.Definition
+{
+    public class TileDefinition
+    {
+        public int Colour { get; set; }
+
+        public int Unknown { get; set; }
+
+        public int ObjectType { get; set; }
+    }
+}
