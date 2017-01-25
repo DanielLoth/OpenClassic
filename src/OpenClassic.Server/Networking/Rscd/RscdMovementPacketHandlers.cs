39f84fdd80d2c43580f48ace4d1e6c974d17e38a
Daniel Loth <lothyis@gmail.com>
2017-01-25 23:25:12 +1100

    Walking message handling using the Path class that was committed earlier.

--- src/OpenClassic.Server/Networking/Rscd/RscdMovementPacketHandlers.cs
+++ src/OpenClassic.Server/Networking/Rscd/RscdMovementPacketHandlers.cs
@@ -1,4 +1,5 @@
 ﻿using DotNetty.Buffers;
+using OpenClassic.Server.Domain;
 using System.Diagnostics;
 
 namespace OpenClassic.Server.Networking.Rscd
@@ -7,9 +8,21 @@ namespace OpenClassic.Server.Networking.Rscd
     {
         public int Opcode => 132;
 
+        private readonly RscdPacketWriter _packetWriter;
+
+        private int _counter = 1;
+
+        public RscdWalkPacketHandler(RscdPacketWriter packetWriter)
+        {
+            Debug.Assert(packetWriter != null);
+
+            _packetWriter = packetWriter;
+        }
+
         public void Handle(ISession session, IByteBuffer packet)
         {
             HandleWalkPacket(session, packet);
+            _packetWriter.SendMessage(session, $"Walk message {_counter++} received");
         }
 
         public static void HandleWalkPacket(ISession session, IByteBuffer packet)
@@ -20,18 +33,21 @@ namespace OpenClassic.Server.Networking.Rscd
             var player = session.Player;
             Debug.Assert(player != null);
 
-            player.StartX = packet.ReadShort();
-            player.StartY = packet.ReadShort();
-
+            var startX = packet.ReadShort();
+            var startY = packet.ReadShort();
             var stepCount = packet.ReadableBytes / 2;
+
+            var xOffsets = new byte[stepCount];
+            var yOffsets = new byte[stepCount];
 
             for (var i = 0; i < stepCount; i++)
             {
-                player.XOffsets[i] = packet.ReadByte();
-                player.YOffsets[i] = packet.ReadByte();
+                xOffsets[i] = packet.ReadByte();
+                yOffsets[i] = packet.ReadByte();
             }
 
-            player.StepCount = stepCount;
+            var path = new Path(startX, startY, xOffsets, yOffsets);
+            player.SetPath(path);
 
             Debug.Assert(packet.ReadableBytes == 0);
         }
